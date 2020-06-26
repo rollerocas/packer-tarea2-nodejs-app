505566b4db4357945d61bed3fdb6b75fd09b93eb
Ricardo Ollero <rollerocas@gmail.com>
2020-06-26 19:25:52 +0200

    change response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('App NodeJs de ejemplo para tarea2-Packer de Herramientas DevOps ## Ricardo Ollero Castela');
+  res.send('App NodeJs de ejemplo para tarea2-Packer de Herramientas DevOps ## Ricardo Ollero Castela ##');
 });
 
 app.listen(3000, function () {
