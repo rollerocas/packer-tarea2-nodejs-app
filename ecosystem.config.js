5c22b5bd7fb2b0a4f2648851d4bac89160524ed2
Ricardo Ollero <rollerocas@gmail.com>
2020-06-27 02:49:41 +0200

    ecosystem changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,12 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+  apps: [{
+    name: "packer-tarea2-nodejs-app",
+    script: "./index.js",
+    env: {
+      NODE_ENV: "production",
+    },
+    env_production: {
+      NODE_ENV: "production",
     }
-  }
+  }]
 };
